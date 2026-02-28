47b9af718be4fb3eb89084d345450833d8e70657
priyamtewari1674 <priyamtewari1674@gmail.com>
2026-02-28 22:42:45 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,6 +38,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 server.listen(PORT, () => {
-  console.log("server up and running on PORT: " + PORT);
+  console.log("server is running on PORT: " + PORT);
   connectDB();
 });
